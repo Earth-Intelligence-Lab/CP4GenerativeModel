0a4b696c520caf80e3abef8229cb4c7f250714e2
Julie <qz1086@nyu.edu>
2025-02-17 15:56:43 -0500

    debug EM and KMeans, unify volume computing

--- EM.py
+++ EM.py
@@ -1,51 +1,54 @@
 import numpy as np
 from tqdm import tqdm
+from scipy.stats import norm
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from matplotlib.patches import Ellipse
 from scipy.stats import multivariate_normal
 from sklearn.mixture import GaussianMixture
 
+
+def gaussian_mixture_density(x, means, variances, weights):
+    density = np.zeros_like(x, dtype=float)
+    for mean, var, weight in zip(means, variances, weights):
+        density += weight * norm.pdf(x, loc=mean, scale=np.sqrt(var)).squeeze()
+    return density
+
+
 def score_fun_EM(ys, y_hat, k_hat, eps=1e-6):
-
-    # ys: (N_ens, d);  y_hat: (d,)
-    # k_hat: scalar, number of clusters in k-NN
-
-    d = ys.shape[1]
+    """
+    ys: (N_ens, d);  y_hat: (d,)
+    k_hat: scalar, number of clusters in k-NN
+    """
     gmm = GaussianMixture(n_components=k_hat, random_state=42, reg_covar=eps)
     gmm.fit(ys)
     means = gmm.means_
     weights = gmm.weights_
     covariances = gmm.covariances_
-
-    distances = []
-    for i in range(k_hat):
-        distance = - (multivariate_normal.logpdf(y_hat, mean=means[i], cov=covariances[i], allow_singular=True) + np.log(weights[i]))
-        distances.append(distance)
-
-    return min(distances)
+    score = - gaussian_mixture_density(y_hat, means, covariances, weights) 
+    return score
 
 def summary_score_EM(data_ys, data_y_hat, k_hat):
-    # data: (N_train, N_ens, d)
-    # k_hat: scalar
-
+    """
+    data: (N_train, N_ens, d)
+    k_hat: scalar
+    """
     scores = []
     for idx, (ys, y_hat) in enumerate(zip(data_ys, data_y_hat)):
         s = score_fun_EM(ys, y_hat, k_hat)
         scores.append(s)
-    return scores
+    return np.array(scores)
 
 
 # given k generated data, find prediction set with 1-alpha percent coverage rate on average
 def inference_EM(ys, y_hat, k_hat, qt, eps=1e-6, grid_res=100):
-
-    # ys: (N_ens, d)
-    # y_hat: (d,)
-    # k_hat: scalar
-    # qt: scalar
-
+    """
+    ys: (N_ens, d)
+    y_hat: (d,)
+    k_hat: scalar
+    qt: scalar
+    """
     d = ys.shape[1]
-
     if len(ys) != k_hat:
       kmeans = KMeans(n_clusters=k_hat, random_state=0).fit(ys)
       means = kmeans.cluster_centers_
@@ -56,60 +59,50 @@ def inference_EM(ys, y_hat, k_hat, qt, eps=1e-6, grid_res=100):
       weights = [1/len(ys) for i in range(len(ys))]
       covariances = [eps * np.eye(d) for i in range(len(ys))]
 
-    # Gaussian KDE prediction sets
-    if d == 2:
-        buffle = (np.max(ys[:,0]) - np.min(ys[:,0])) * 0.1
-        x, y = np.meshgrid(np.linspace(np.min(ys[:,0])-buffle, np.max(ys[:,0])+buffle, grid_res), np.linspace(np.min(ys[:,1])-buffle, np.max(ys[:,1])+buffle, grid_res))
-        pos = np.dstack((x, y))
-        dens = np.zeros((k_hat, x.shape[0], x.shape[1]), float)
+    # compute score
+    score = - gaussian_mixture_density(y_hat, means, covariances, weights)
+    volume = get_coverage_length_brutal(ys, means, covariances, weights, qt, k_hat, grid_res)
+    return score, volume
 
-        for i in range(k_hat):
-            dens[i] = - (multivariate_normal.logpdf(pos, mean=means[i], cov=covariances[i], allow_singular=True) + np.log(weights[i]))
-        dens = np.min(dens, axis=0)
-
-        dens[dens <= qt] = 1
-        dens[dens > qt] = 0
-
-        # compute score
-        scores = []
-        for i in range(k_hat):
-            score = - (multivariate_normal.logpdf(y_hat, mean=means[i], cov=covariances[i], allow_singular=True) + np.log(weights[i]))
-            scores.append(score)
-
-        volume = (x.shape[0] * x.shape[1] - np.sum(dens)) * (x[0,1] - x[0,0]) * (y[1,0] - y[0,0])
-
-    elif d == 1:
+# compute volume for a single sample through MC
+def get_coverage_length_brutal(ys, means, covariances, weights, qt, k_hat, grid_res=100):
+    d = ys.shape[1]
+    if d == 1:
         buffle = (np.max(ys[:,0]) - np.min(ys[:,0])) * 0.1
         x = np.linspace(np.min(ys[:,0])-buffle, np.max(ys[:,0])+buffle, grid_res)
-        dens = np.zeros((k_hat, x.shape[0]), float)
-        for i in range(k_hat):
-            dens[i] = - (multivariate_normal.logpdf(x, mean=means[i], cov=covariances[i], allow_singular=True) + np.log(weights[i]))
-        dens = np.min(dens, axis=0) # min over k_hat----we could also take the summation?
-        dens[dens <= qt] = 1
-        dens[dens > qt] = 0
+        dens = - gaussian_mixture_density(x, means, covariances, weights)
 
-        # compute score
-        scores = []
-        for i in range(k_hat):
-            score = - (multivariate_normal.logpdf(y_hat, mean=means[i], cov=covariances[i], allow_singular=True) + np.log(weights[i]))
-            scores.append(score)
+        dens_new = np.zeros(dens.shape) * np.nan
+        dens_new[dens <= qt] = 1
+        dens_new[dens > qt] = 0
+        volume = (np.sum(dens_new)) * (x[1] - x[0])
 
-        volume = np.sum(dens) * (x[1] - x[0])
-    return min(scores), dens, volume
+    elif d == 2:
+        buffle0 = (np.max(ys[:,0]) - np.min(ys[:,0])) * 0.1
+        buffle1 = (np.max(ys[:,1]) - np.min(ys[:,1])) * 0.1
+        x, y = np.meshgrid(np.linspace(np.min(ys[:,0])-buffle0, np.max(ys[:,0])+buffle0, grid_res), \
+                           np.linspace(np.min(ys[:,1])-buffle1, np.max(ys[:,1])+buffle1, grid_res))
+        pos = np.dstack((x, y))
 
-
+        dens = - gaussian_mixture_density(pos, means, covariances, weights)
+        
+        dens_new = np.zeros(dens.shape) * np.nan
+        dens_new[dens <= qt] = 1
+        dens_new[dens > qt] = 0
+        volume = (np.sum(dens_new)) * (x[0,1] - x[0,0]) * (y[1,0] - y[0,0])
+    return volume
 
 def summary_inference_EM(data_ys, data_y_hat, k_hat, qt, grid_res=200):
-
-    # data: (N_test, N_ens, d)
-    # k_hat: scalar, number of clusters in k-NN
-    # qt: scalar, quantile corresponding to the coverage rate
-
+    """
+    data: (N_test, N_ens, d)
+    k_hat: scalar, number of clusters in k-NN
+    qt: scalar, quantile corresponding to the coverage rate
+    """
     scores = []
     volumes = []
 
     for idx, (ys, y_hat) in enumerate(zip(data_ys, data_y_hat)):
-        score, dens, volume = inference_EM(ys, y_hat, k_hat=k_hat, qt=qt, grid_res=grid_res)
+        score, volume = inference_EM(ys, y_hat, k_hat=k_hat, qt=qt, grid_res=grid_res)
 
         scores.append(score)
         volumes.append(volume)
--- KMeans.py
+++ KMeans.py
@@ -1,15 +1,18 @@
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from scipy.special import gamma
 from sklearn.cluster import KMeans
 from matplotlib.patches import Ellipse
 from scipy.stats import multivariate_normal
 
+
+# compute score for single sample
 def score_fun_KMeans(ys, y_hat, k_hat, eps=1e-6):
-
-    # ys: (N_ens, d);  y_hat: (d,)
-    # k_hat: scalar, number of clusters in k-NN
-
+    """
+    ys: (N_ens, d);  y_hat: (d,)
+    k_hat: scalar, number of clusters in k-NN
+    """
     d = ys.shape[1]
     if len(ys) != k_hat: # number of clusters less than number of ensemble members
       kmeans = KMeans(n_clusters=k_hat, random_state=0).fit(ys)
@@ -28,10 +31,11 @@ def score_fun_KMeans(ys, y_hat, k_hat, eps=1e-6):
 
     return min(distances)
 
+
+# compute score for multiple samples
 def summary_score_KMeans(data_ys, data_y_hat, k_hat):
     # data: (N_train, N_ens, d)
     # k_hat: scalar
-
     scores = []
     for idx, (ys, y_hat) in enumerate(zip(data_ys, data_y_hat)):
         s = score_fun_KMeans(ys, y_hat, k_hat)
@@ -39,16 +43,15 @@ def summary_score_KMeans(data_ys, data_y_hat, k_hat):
     return scores
 
 
-# given k generated data, find prediction set with 1-alpha percent coverage rate on average
+# find score & prediction set volume for a single sample with 1-alpha percent coverage rate on average
 def inference_KMeans(ys, y_hat, k_hat, qt, eps=1e-6, grid_res=100):
-
-    # ys: (N_ens, d)
-    # y_hat: (d,)
-    # k_hat: scalar
-    # qt: scalar
-
+    """
+    ys: (N_ens, d)
+    y_hat: (d,)
+    k_hat: scalar
+    qt: scalar
+    """
     d = ys.shape[1]
-
     if len(ys) != k_hat:
       kmeans = KMeans(n_clusters=k_hat, random_state=0).fit(ys)
       means = kmeans.cluster_centers_
@@ -59,60 +62,120 @@ def inference_KMeans(ys, y_hat, k_hat, qt, eps=1e-6, grid_res=100):
       weights = [1/len(ys) for i in range(len(ys))]
       covariances = [eps * np.eye(d) for i in range(len(ys))]
 
-    # Gaussian KDE prediction sets
-    if d == 2:
-        buffle = (np.max(ys[:,0]) - np.min(ys[:,0])) * 0.1
-        x, y = np.meshgrid(np.linspace(np.min(ys[:,0])-buffle, np.max(ys[:,0])+buffle, grid_res), np.linspace(np.min(ys[:,1])-buffle, np.max(ys[:,1])+buffle, grid_res))
-        pos = np.dstack((x, y))
-        dens = np.zeros((k_hat, x.shape[0], x.shape[1]), float)
-
+    # compute score for the sample
+    scores = []
+    for i in range(k_hat):
+        score = - (multivariate_normal.logpdf(y_hat, mean=means[i], cov=covariances[i], allow_singular=True) + np.log(weights[i]))
+        scores.append(score)
+    score = min(scores)
+    # print('weights: ', weights)
+    # print('covariances: ', covariances)
+    # compute volume for the prediction set
+    if d == 1:
+        radius = []
         for i in range(k_hat):
-            dens[i] = - (multivariate_normal.logpdf(pos, mean=means[i], cov=covariances[i], allow_singular=True) + np.log(weights[i]))
-        dens = np.min(dens, axis=0)
+            sigma = covariances[i][0]**0.5
+            # print(2*qt - np.log( 2*np.pi) - 2*np.log(sigma / weights[i] ))
+            radius.append(sigma * np.sqrt( max( 0, 2*qt - np.log( 2*np.pi) - 2*np.log(sigma / weights[i]) ) ) )
+        volume = get_coverage_length_exact(radius, means)
+    elif d == 2:
+        volume = get_coverage_length_brutal(ys, means, covariances, weights, qt, k_hat, grid_res)
 
-        dens[dens <= qt] = 1
-        dens[dens > qt] = 0
+    return score, volume
 
-        # compute score
-        scores = []
-        for i in range(k_hat):
-            score = - (multivariate_normal.logpdf(y_hat, mean=means[i], cov=covariances[i], allow_singular=True) + np.log(weights[i]))
-            scores.append(score)
 
-        volume = (x.shape[0] * x.shape[1] - np.sum(dens)) * (x[0,1] - x[0,0]) * (y[1,0] - y[0,0])
-
-    elif d == 1:
+# compute volume for a single sample through MC
+def get_coverage_length_brutal(ys, means, covariances, weights, qt, k_hat, grid_res=100):
+    d = ys.shape[1]
+    if d == 1:
         buffle = (np.max(ys[:,0]) - np.min(ys[:,0])) * 0.1
         x = np.linspace(np.min(ys[:,0])-buffle, np.max(ys[:,0])+buffle, grid_res)
         dens = np.zeros((k_hat, x.shape[0]), float)
         for i in range(k_hat):
-            dens[i] = - (multivariate_normal.logpdf(x, mean=means[i], cov=covariances[i], allow_singular=True) + np.log(weights[i]))
+            dens[i] = - (multivariate_normal.logpdf(x, mean=means[i], cov=covariances[i], allow_singular=True)\
+                      + np.log(weights[i]))
         dens = np.min(dens, axis=0) # min over k_hat----we could also take the summation?
-        dens[dens <= qt] = 1
-        dens[dens > qt] = 0
 
-        # compute score
-        scores = []
+        dens_new = np.zeros(dens.shape) * np.nan
+        dens_new[dens <= qt] = 1
+        dens_new[dens > qt] = 0
+        volume = (np.sum(dens_new)) * (x[1] - x[0])
+
+    elif d == 2:
+        buffle0 = (np.max(ys[:,0]) - np.min(ys[:,0])) * 0.1
+        buffle1 = (np.max(ys[:,1]) - np.min(ys[:,1])) * 0.1
+        x, y = np.meshgrid(np.linspace(np.min(ys[:,0])-buffle0, np.max(ys[:,0])+buffle0, grid_res), \
+                           np.linspace(np.min(ys[:,1])-buffle1, np.max(ys[:,1])+buffle1, grid_res))
+        pos = np.dstack((x, y))
+        dens = np.zeros((k_hat, x.shape[0], x.shape[1]), float)
+
         for i in range(k_hat):
-            score = - (multivariate_normal.logpdf(y_hat, mean=means[i], cov=covariances[i], allow_singular=True) + np.log(weights[i]))
-            scores.append(score)
+            dens[i] = - (multivariate_normal.logpdf(pos, mean=means[i], cov=covariances[i], allow_singular=True)\
+                      + np.log(weights[i]))
+        dens = np.min(dens, axis=0)
+        
+        dens_new = np.zeros(dens.shape) * np.nan
+        dens_new[dens <= qt] = 1
+        dens_new[dens > qt] = 0
+        volume = (np.sum(dens_new)) * (x[0,1] - x[0,0]) * (y[1,0] - y[0,0])
+    return volume
 
-        volume = np.sum(dens) * (x[1] - x[0])
-    return min(scores), dens, volume
+
+# compute coverage length for a single sample exactly
+def get_coverage_length_exact(radius,Y_test):
+    """
+    Y_test in the shape of (n_sample, d)
+    """
+    def get_overlap_length(intervals):
+        temp_tuple = intervals
+        temp_tuple.sort(key=lambda interval: interval[0])
+        merged = [temp_tuple[0]]
+        for current in temp_tuple:
+            previous = merged[-1]
+            if current[0] <= previous[1]:
+                previous[1] = max(previous[1], current[1])
+            else:
+                merged.append(current)
+        l=0
+        for x in merged:
+            l+= x[1]-x[0]
+        return l 
+    
+    n_sample = Y_test.shape[0]
+    if isinstance(radius, float):
+        radius = [radius]*n_sample
+
+    I = []
+    for j in range(n_sample):
+        l = (Y_test[j]-radius[j])
+        u = (Y_test[j]+radius[j])
+        I.append([l,u])
+    coverage_length=get_overlap_length(I)
+    return coverage_length
 
 
+def get_coverage_volume_overlap(means, covariances, weights, k_hat, qt):
+    volumes = []
+    d = means.shape[1]
+    for i in range(k_hat):
+        det = np.linalg.det(covariances[i])
+        r = np.sqrt( 2*qt - d*np.log(2*np.pi)-np.log(det) + 2*np.log(weights[i]) )
+        volumes.append(np.pi**(d/2) * r**d / gamma(d/2+1) * np.sqrt(det))
+    return np.sum(volumes)
 
+
+# compute score & prediction set volume for multiple samples
 def summary_inference_KMeans(data_ys, data_y_hat, k_hat, qt, grid_res=200):
-
-    # data: (N_test, N_ens, d)
-    # k_hat: scalar, number of clusters in k-NN
-    # qt: scalar, quantile corresponding to the coverage rate
-
+    """
+    data: (N_test, N_ens, d)
+    k_hat: scalar, number of clusters in k-NN
+    qt: scalar, quantile corresponding to the coverage rate
+    """
     scores = []
     volumes = []
 
     for idx, (ys, y_hat) in enumerate(zip(data_ys, data_y_hat)):
-        score, dens, volume = inference_KMeans(ys, y_hat, k_hat=k_hat, qt=qt, grid_res=grid_res)
+        score, volume = inference_KMeans(ys, y_hat, k_hat=k_hat, qt=qt, grid_res=grid_res)
 
         scores.append(score)
         volumes.append(volume)
